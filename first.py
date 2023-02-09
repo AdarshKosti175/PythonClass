8f3a0128a32068d8cfd03aa962c9f253e716e634
AdarshKosti175 <adarshkosti697@gmail.com>
2023-02-09 23:23:04 +0530

    First Commit

---
+++ first.py
@@ -0,0 +1,18 @@
+"""distance_km = 500   distance travelled
+time_taken = 4      time duration
+speed = distance_km / time_taken
+print(speed)"""
+
+#Arithmetic Program#
+a = 10
+b = 15
+c = a + b
+print(c)
+d = a - b
+print(d) 
+e = a * b
+print(e)
+f = a / b
+print(f)
+g = a++b
+print(g) 
